297034d133beac65edf8489cde04f34212728a16
Amit <amit.hansda@in.pwc.com>
2016-12-10 12:05:53 +0530

    Initial Commit

---
+++ src/Hackathon/ProjectHappyFace/ProjectHappyFace.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjectHappyFace.UI.MvcApplication" Language="C#" %>
---
+++ src/Hackathon/ProjectHappyFace/ProjectHappyFace.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectHappyFace.UI.Startup))]
+namespace ProjectHappyFace.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
